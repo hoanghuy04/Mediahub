082d8c8b11b6c31b5d397efe4202563618801ee0
TranNgocHuyen19 <120317245+TranNgocHuyen19@users.noreply.github.com>
2026-02-20 11:34:22 +0700

    feat(notification-service): [BH-147] centralize notification orchestrator

--- notification-service/src/main/java/com/bondhub/notificationservices/service/notification/NotificationServiceImpl.java
+++ notification-service/src/main/java/com/bondhub/notificationservices/service/notification/NotificationServiceImpl.java
@@ -5,10 +5,13 @@ import com.bondhub.notificationservices.dto.response.notification.NotificationRe
 import com.bondhub.notificationservices.enums.NotificationType;
 import com.bondhub.notificationservices.mapper.NotificationMapper;
 import com.bondhub.notificationservices.model.Notification;
+import com.bondhub.notificationservices.orchestrator.NotificationOrchestrator;
 import com.bondhub.notificationservices.repository.NotificationRepository;
 import com.bondhub.notificationservices.strategy.content.NotificationContentStrategy;
 import com.bondhub.notificationservices.strategy.content.factory.ContentStrategyFactory;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -16,25 +19,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class NotificationServiceImpl implements NotificationService {
 
-    ContentStrategyFactory contentStrategyFactory;
-    NotificationRepository notificationRepository;
-    NotificationMapper notificationMapper;
+    NotificationOrchestrator orchestrator;
+    NotificationMapper mapper;
 
     @Override
-    public NotificationResponse createFriendRequestNotification(
-            CreateFriendRequestNotificationRequest request) {
-
-        log.info("Creating friend request notification for receiver={}",
-                request.receiverId());
-
-        NotificationContentStrategy strategy = contentStrategyFactory.get(NotificationType.FRIEND_REQUEST);
-
-        Notification notification = strategy.build(request);
-
-        Notification saved = notificationRepository.save(notification);
-
-        return notificationMapper.toResponse(saved);
+    public NotificationResponse createFriendRequestNotification(CreateFriendRequestNotificationRequest request) {
+        log.info("Creating friend request notification");
+        Notification saved = orchestrator.process( NotificationType.FRIEND_REQUEST, request);
+        return mapper.toResponse(saved);
     }
 }
