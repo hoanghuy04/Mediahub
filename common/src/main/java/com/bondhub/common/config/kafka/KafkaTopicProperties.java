41a68bed3fb56f5fedc97abd4d2fbee9600feafc
DaoThiMyLinh <daolinh28102004@gmail.com>
2026-03-03 16:20:16 +0700

    feat(user-block): [BH-127] implement interceptor to restrict communication when blocked

--- common/src/main/java/com/bondhub/common/config/kafka/KafkaTopicProperties.java
+++ common/src/main/java/com/bondhub/common/config/kafka/KafkaTopicProperties.java
@@ -3,9 +3,7 @@ package com.bondhub.common.config.kafka;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "kafka.topics")
 @Getter
 @Setter
